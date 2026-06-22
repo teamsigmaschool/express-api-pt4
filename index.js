eecf731410b16e61016f9fcdeebe1bd43d8bc572
Reagan Kan <mjkan96@gmail.com>
2026-06-22 17:43:27 +0800

    Update on index.js from slides

--- index.js
+++ index.js
@@ -174,9 +174,15 @@ app.get("/likes/post/:post_id", async (req, res) => {
   const { post_id } = req.params;
   const client = await pool.connect();
   try {
-    const likes = await client.query("SELECT * FROM likes WHERE post_id = $1", [
-      post_id,
-    ]);
+    const likes = await client.query(
+      `
+    SELECT users.username, users.id AS user_id, likes.id AS likes_id
+    FROM likes
+    INNER JOIN users ON likes.user_id = users.id
+    WHERE likes.post_id = $1
+  `,
+      [post_id],
+    );
     res.json(likes.rows);
   } catch (err) {
     console.log(err.stack);
